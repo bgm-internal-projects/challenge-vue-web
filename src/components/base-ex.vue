17b161f845ec4803dc06b77d4596ad9cf2503f56
bgm.cod <bgm.codfish@gmail.com>
2024-08-14 15:54:44 +0800

    Update base-ex.vue

--- src/components/base-ex.vue
+++ src/components/base-ex.vue
@@ -5,6 +5,7 @@ import { ref } from 'vue';
 
 // 1
 // 2
+// 3
 
 interface Props {
   label?: string;
@@ -12,8 +13,6 @@ interface Props {
 const props = withDefaults(defineProps<Props>(), {
   label: '',
 });
-
-// old 3
 
 const emit = defineEmits<{
   (e: 'update:modelValue', value: string): void;
