a3b12354ce714c9f4c725e52ddc0b8f9a537d467
bgm.cod <bgm.codfish@gmail.com>
2025-05-16 18:31:55 +0800

    feat: 基本邏輯完成

--- src/components/q-menu-hover/q-menu-hover.vue
+++ src/components/q-menu-hover/q-menu-hover.vue
@@ -15,11 +15,8 @@
 <script setup lang="ts">
 import { useElementHover, useParentElement } from '@vueuse/core';
 import { debounce, QMenu, QMenuProps } from 'quasar';
-import { triggerRef } from 'vue';
-import { shallowRef } from 'vue';
 import { onBeforeUnmount } from 'vue';
-import { onBeforeMount } from 'vue';
-import { computed, provide, inject, ref, watch, nextTick, InjectionKey, onMounted, Ref } from 'vue';
+import { computed, provide, inject, ref, watch } from 'vue';
 import { injectionKey } from './constant';
 
 interface Submenu {
@@ -33,16 +30,15 @@ const props = withDefaults(defineProps<Props>(), {});
 const id = crypto.randomUUID()
 
 const triggerEl = useParentElement()
-
 const isTriggerHover = useElementHover(triggerEl, {
-  delayLeave: 200,
+  delayLeave: 100,
 })
 
 const menuRef = ref<InstanceType<typeof QMenu>>()
 const isMenuHover = ref(false)
 const handleMenuLeave = debounce(() => {
   isMenuHover.value = false
-}, 200)
+}, 100)
 function handleMenuHover() {
   isMenuHover.value = true
   handleMenuLeave.cancel()
@@ -52,34 +48,57 @@ function handleMenuHover() {
 //   console.log('🚀 ~ isMenuHover:', isMenuHover);
 // })
 
+const submenuList = ref<Submenu[]>([])
+
 const rootProvider = inject(injectionKey, null)
+const currentSubmenuList = computed(() => rootProvider?.submenuList.value ?? [])
+// watch(currentSubmenuList, () => {
+//   console.log('🚀 ~ [watch] currentSubmenuList:', currentSubmenuList);
+// }, { deep: true })
+
+const menuLevel = computed(() => {
+  if (!rootProvider) {
+    return 0
+  }
+
+  const index = currentSubmenuList.value.findIndex((item) => item.id === id)
+  return index < 0 ? undefined : index + 1
+})
 
 const hasSubmenuVisible = computed(() => {
-  const { submenuList } = rootProvider ?? {}
-  if (!submenuList) {
+  if (menuLevel.value === 0 && submenuList.value.length > 0) {
+    return true
+  }
+
+  const level = menuLevel.value
+  if (!level) {
     return false
   }
 
-  const index = submenuList.value.findIndex((item) => item.id === id)
-  if (index === -1) {
-    return false
-  }
-
-  return index < submenuList.value.length - 1
+  return level < currentSubmenuList.value.length - 1
 })
 
 const menuVisible = ref(false)
 watch(() => [
   isTriggerHover, isMenuHover, hasSubmenuVisible
 ], () => {
+  // if (!rootProvider) {
+  //   console.log('\n🚀 ~ root:');
+  // } else {
+  //   console.log('\n🚀 ~ child:');
+  // }
+
+  // console.log('🚀 ~ isTriggerHover:', isTriggerHover);
+  // console.log('🚀 ~ isMenuHover:', isMenuHover);
+  // console.log('🚀 ~ hasSubmenuVisible:', hasSubmenuVisible);
+  // console.log('🚀 ~ menuLevel:', menuLevel);
+  // console.log('🚀 ~ currentSubmenuList length:', currentSubmenuList.value.length);
+
   menuVisible.value = isTriggerHover.value || isMenuHover.value || hasSubmenuVisible.value
 }, { deep: true })
 
 
 watch(menuVisible, (value) => {
-  console.log(`🚀 ~ triggerEl: `, triggerEl);
-  console.log(`🚀 ~ ${id}:`, value);
-  console.table({ ...rootProvider?.submenuList.value });
   if (value) {
     rootProvider?.bindSubmenu({ id })
   } else {
@@ -87,7 +106,29 @@ watch(menuVisible, (value) => {
   }
 })
 
+// onMounted(() => {
+//   console.log('🚀 ~ rootProvider:', rootProvider);
+// })
+
 onBeforeUnmount(() => {
   rootProvider?.unbindSubmenu(id)
 })
+
+// rootProvider 邏輯
+function bindSubmenu(data: Submenu) {
+  submenuList.value.push(data)
+}
+function unbindSubmenu(id: string) {
+  const index = submenuList.value.findIndex((item) => item.id === id)
+  if (index !== -1) {
+    submenuList.value.splice(index, 1)
+  }
+}
+if (!rootProvider) {
+  provide(injectionKey, {
+    bindSubmenu,
+    unbindSubmenu,
+    submenuList,
+  })
+}
 </script>
--- src/views/the-dropdowns-enabled-on-hover.vue
+++ src/views/the-dropdowns-enabled-on-hover.vue
@@ -1,80 +1,77 @@
 <template>
   <div class="flex gap-10 p-10">
-    <q-menu-hover-root>
-      <q-btn
-        label="Menu"
-        color="primary"
-      >
-        <q-menu-hover>
-          <q-list
-            dense
-            style="min-width: 100px"
-          >
-            <q-item
-              v-close-popup
-              clickable
-            >
-              <q-item-section>Open...</q-item-section>
-            </q-item>
-            <q-item
-              v-close-popup
-              clickable
-            >
-              <q-item-section>New</q-item-section>
-            </q-item>
-            <q-separator />
-            <q-item clickable>
-              <q-item-section>Preferences</q-item-section>
-              <q-item-section side>
-                <q-icon name="keyboard_arrow_right" />
-              </q-item-section>
+    <div class="p-4 bg-primary text-white rounded">
+      Menu
 
-              <q-menu-hover
-                anchor="top end"
-                self="top start"
-              >
-                <q-list>
-                  <q-item
-                    v-for="n in 3"
-                    :key="n"
-                    dense
-                    clickable
-                  >
-                    <q-item-section>Submenu Label</q-item-section>
-                    <q-item-section side>
-                      <q-icon name="keyboard_arrow_right" />
-                    </q-item-section>
-                    <q-menu-hover
-                      auto-close
-                      anchor="top end"
-                      self="top start"
-                    >
-                      <q-list ref="list">
-                        <q-item
-                          v-for="n in 3"
-                          :key="n"
-                          dense
-                          clickable
-                        >
-                          <q-item-section>3rd level Label</q-item-section>
-                        </q-item>
-                      </q-list>
-                    </q-menu-hover>
-                  </q-item>
-                </q-list>
-              </q-menu-hover>
-            </q-item>
-            <q-separator />
-            <q-item
-              v-close-popup
-              clickable
+      <q-menu-hover>
+        <q-list
+          dense
+          style="min-width: 100px"
+        >
+          <q-item
+            v-close-popup
+            clickable
+          >
+            <q-item-section>Open...</q-item-section>
+          </q-item>
+          <q-item
+            v-close-popup
+            clickable
+          >
+            <q-item-section>New</q-item-section>
+          </q-item>
+          <q-separator />
+          <q-item clickable>
+            <q-item-section>Preferences</q-item-section>
+            <q-item-section side>
+              <q-icon name="keyboard_arrow_right" />
+            </q-item-section>
+
+            <q-menu-hover
+              anchor="top end"
+              self="top start"
             >
-              <q-item-section>Quit</q-item-section>
-            </q-item>
-          </q-list>
-        </q-menu-hover>
-      </q-btn>
-    </q-menu-hover-root>
+              <q-list>
+                <q-item
+                  v-for="n in 3"
+                  :key="n"
+                  dense
+                  clickable
+                >
+                  <q-item-section>Submenu Label</q-item-section>
+                  <q-item-section side>
+                    <q-icon name="keyboard_arrow_right" />
+                  </q-item-section>
+                  <q-menu-hover
+                    auto-close
+                    anchor="top end"
+                    self="top start"
+                  >
+                    <q-list ref="list">
+                      <q-item
+                        v-for="n in 1"
+                        :key="n"
+                        dense
+                        clickable
+                      >
+                        <q-item-section>3rd level Label</q-item-section>
+                      </q-item>
+                    </q-list>
+                  </q-menu-hover>
+                </q-item>
+              </q-list>
+            </q-menu-hover>
+          </q-item>
+          <q-separator />
+          <q-item
+            v-close-popup
+            clickable
+          >
+            <q-item-section>Quit</q-item-section>
+          </q-item>
+        </q-list>
+      </q-menu-hover>
+    </div>
 
     <q-btn
       label="Menu"
@@ -153,11 +150,7 @@
 </template>
 
 <script setup lang="ts">
-import { computed, ref, watch } from 'vue';
 import QMenuHover from '../components/q-menu-hover/q-menu-hover.vue';
-import QMenuHoverRoot from '../components/q-menu-hover/q-menu-hover-root.vue';
-
-
 </script>
 
 <style scoped lang="sass">
